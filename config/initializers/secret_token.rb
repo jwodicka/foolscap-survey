bf107861349e50d2164566019fa1c35e52d6bf75
Jack Foy <jfoy@whitepages.com>
2012-06-20 22:47:41 -0700

    Initial commit: bare Rails stub app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoolscapSurvey::Application.config.secret_token = 'ceb617e19dd689cd407f2235cf948a9d10462bd3bdc35f8b2654f55c39a10f713d34555a6cf043796189835481d2e888ca0ef23c2619ff2ad517455d4cdaa92e'
